7b7f8dfce66fef9b62baa83478580185cc375d8f
koceilabeast <6303016@cegepmv.ca>
2025-12-10 17:30:06 -0500

    created test class for OffreService

---
+++ src/test/java/app/reservationsystem/service/OffreServiceTest.java
@@ -0,0 +1,18 @@
+package app.reservationsystem.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OffreServiceTest {
+
+    @Test
+    void getOffrePrixMin() {
+
+    }
+
+    @Test
+    void getOffrePrixMax() {
+
+    }
+}
